35eb50d87978a6442bf25eb885dc89d7be4892cd
S M <malapit66@gmail.com>
2020-09-06 00:57:30 -0500

    creating template system

--- index.js
+++ index.js
@@ -6,6 +6,17 @@ const app = express();
 
 const PORT = process.env.PORT || 23795;
 
+const ignoredPaths = [
+  '/template.html'
+];
+
+app.use((req, res, next) => {
+  if (ignoredPaths.includes(req.path)) {
+    res.status(404).redirect('/404');
+  }
+  next();
+});
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(PORT, (error) => {
