547c6f12aac066c67de0ff2e16fa3a3e4b3267c3
S M <malapit66@gmail.com>
2020-09-08 00:28:15 -0500

    routine push

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -3,18 +3,31 @@ const terminal = document.getElementById('terminal');
 
 const socket = io(window.location.origin);
 const cmdsCache = [];
+const tempNum = 0;
+let cmdsIndex = {
+  _num: 0,
+  get num() {
+    return this._num;
+  },
+  set num(value) {
+    return this._num = constrain(value, 0, cmdsCache.length);
+  },
+  valueOf() {
+    return this._num;
+  }
+};
 
 cmdLine.select();
 
-cmdLine.addEventListener('keydown', (event) => {
-  if (event.key !== 'Enter') return;
+cmdLine.addEventListener('keydown', ({ key }) => {
+  if (key !== 'Enter') return;
 
   const text = cmdLine.value;
   cmdLine.value = null;
 
   if (!!text.replace(/\s/g, '').length) {
     print(text);
-    cmdsCache.push(text);
+    cmdsCache.unshift(text);
   }
 
   if (text[0] === '/') parseCommand(text);
@@ -40,4 +53,8 @@ function print(text) {
   line.append(lineText);
 
   terminal.insertBefore(line, cmdLine);
+}
+
+function constrain(num, limit1, limit2) {
+  return num < limit1 ? limit1 : num > limit2 ? limit2 : num;
 }
--- routes.js
+++ routes.js
@@ -5,28 +5,30 @@ const fs = require('fs');
 const router = express.Router();
 
 router.use(({ path }, res, next) => {
-  if (/\.template\.html/.test(path)) {
+  if (/\.?template\.html/.test(path)) {
     return res.status(404).end();
   }
   next();
 });
 
-router.use(async (req, res, next) => {
-  const filename = (req.path.match(/\/(.*)\.html/) || [])[1];
-  const htmlName = filename + '.template.html';
+router.use((req, res, next) => {
+  return Promise.resolve().then(() => {
+    const filename = (req.path.match(/\/(.*)\.html/) || [])[1];
+    const htmlName = filename + '.template.html';
 
-  if (!fs.existsSync(path.join('public', htmlName))) { return next(); }
+    if (!fs.existsSync(path.join('public', htmlName))) { return next(); }
 
-  const file = fs.readFileSync(path.join('public', htmlName)).toString();
-  const regex = /<head>(?<head>[\s\S]*)<\/head>[\s\S]*<body>(?<body>[\s\S]*)<\/body>/;
-  const { head, body } = file.match(regex).groups;
-  const templatedFile = fs.readFileSync(path.join('public', 'template.html'))
-    .toString()
-    .replace('<!-- ##HEAD -->', head.trim())
-    .replace('<!-- ##BODY -->', body.trim())
-  ;
+    const file = fs.readFileSync(path.join('public', htmlName)).toString();
+    const regex = /<head>(?<head>[\s\S]*)<\/head>[\s\S]*<body>(?<body>[\s\S]*)<\/body>/;
+    const { head, body } = file.match(regex).groups;
+    const templatedFile = fs.readFileSync(path.join('public', 'template.html'))
+      .toString()
+      .replace('<!-- ##HEAD -->', head.trim())
+      .replace('<!-- ##BODY -->', body.trim())
+    ;
 
-  res.send(templatedFile);
+    res.send(templatedFile);
+  });
 });
 
 router.get('/', (_, res) => res.redirect('/index.html'));
